88d0ef473b9f256a36341b452c9c1cb2f05f9e26
hustf <hustf@users.noreply.github.com>
2021-03-25 17:28:42 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/MechGlueModelingToolkit.jl
@@ -0,0 +1,5 @@
+module MechGlueModelingToolkit
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using MechGlueModelingToolkit
+using Test
+
+@testset "MechGlueModelingToolkit.jl" begin
+    # Write your tests here.
+end
